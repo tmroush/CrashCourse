8967b97730e75ab88cddb68f0af47c0092795415
Tricia Roush <tricia.roush@gmail.com>
2023-09-28 15:16:23 -0600

    Learning to use if-elif-else.

--- CrashCourse/alien.py
+++ CrashCourse/alien.py
@@ -1,10 +1,16 @@
-alien_0 = {'color': 'green', 'points': 5}
-print(alien_0['color'])
-print(alien_0['points'])
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original posiotion: {alien_0['x_position']}")
 
-print(alien_0)
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
 
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
 
-print(alien_0)
+print(f"New position: {alien_0['x_position']}")
