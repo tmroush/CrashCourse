7dc96d9d44fc38c3b9f3d741c55282c87b8530c6
Tricia Roush <tricia.roush@gmail.com>
2023-10-03 17:05:32 -0600

    Learning to import classes into files and such.

--- CrashCourse/car.py
+++ CrashCourse/car.py
@@ -1,3 +1,4 @@
+"""A class that can be used to represent a car."""
 class Car:
     """A simple attempt to represent a car."""
 
@@ -30,11 +31,6 @@ class Car:
     def increment_odometer(self, miles):
         """Adds the given amount to the odometer reading."""
         self.odometer_reading += miles
-
-    def fill_gas_tank(self):
-        """Simulates filling the gas tank"""
-        print("The tank is now full.")
-
 
 class Battery:
     """A simple attempt to model a battery for an electric car"""
@@ -73,20 +69,3 @@ class ElectricCar(Car):
         """Electric cars don't have gass tanks."""
         print("Electric cars don't have gas tanks.")
 
-
-
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
-
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
